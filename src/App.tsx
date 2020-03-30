51aa8050c87d7276354784f983150ba0da39e3de
MaxMagnuson <magnu401@morris.umn.edu>
2020-03-30 18:18:43 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+import React from 'react';
+import './App.css';
+import { Home } from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;
